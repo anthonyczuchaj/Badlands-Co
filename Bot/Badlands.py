a3d98c4a3f724c1d011f914a22bade43eb7d38bb
anthonyczuchaj <162690419+anthonyczuchaj@users.noreply.github.com>
2026-09-23 13:20:00 -0500

    changes

--- Bot/Badlands.py
+++ Bot/Badlands.py
@@ -4,3 +4,19 @@ embedItemMarketListing = discord.Embed(
     description="Take this oppurtunity to list an item, either for sale, or for auction. Any money you make is yours to keep, and yours to earn!",
     color=discord.Color.gold()
     )
+embedItemMarketListing.set_image(url="/IMG/Banner.png")
+
+embedItemSell = discord.Embed(
+    title="Sell An Item!",
+)
+embedItemAuction = ""
+
+class ItemMarketView(discord.ui.View):
+    
+    @discord.ui.button(label="Sell an Item",style=discord.ButtonStyle.primary)
+    async def sell(self,interaction: discord.Interaction, button: discord.ui.button):
+        await interaction.response.send_message("Sell an Item Clicked!", ephemeral=True)
+    @discord.ui.button(label="Auction an Item",style=discord.ButtonStyle.primary)
+    async def auction(self, interaction: discord.Interaction, button:discord.ui.button):
+        await interaction.response.send_message("Auction an Item Clicked!", ephemeral=False)
+    
--- Bot/Main.py
+++ Bot/Main.py
@@ -7,32 +7,27 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = discord.Object(id=1486148994403930274)
 
 class BadlandsBot(discord.Client):
-    def __init__(self):
-        intents = discord.Intents.default()
-        super().__init__(intents=intents)
 
+    def __init__(self):
+        super().__init__(intents=discord.Intents.default())
         self.tree = app_commands.CommandTree(self)
 
     async def setup_hook(self):
-        guild = discord.Object(id=1486148994403930274)
-        await self.tree.sync(guild=guild)
-        print("Commands synced!")
+        self.tree.copy_global_to(guild=GUILD)
+        await self.tree.sync(guild=GUILD)
 
 bot = BadlandsBot()
 
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user}")
-
-@bot.tree.command(name="createitempost",description="Meant to be used only by admins to create the item market listing post!")
+@bot.tree.command(name="createitempost",description="Create the item market listing post.")
 async def create_item_post(interaction: discord.Interaction):
+
     forumItemMarket = bot.get_channel(1550648832784736366)
-    if forumItemMarket:
-        await forumItemMarket.create_thread(
-            name="==-->List an Item<--==", 
-            embed=Badlands.embedItemMarketListing
-            )
+    await forumItemMarket.create_thread(name="==-->List an Item<--==",embed=Badlands.embedItemMarketListing,view=Badlands.ItemMarketView())
+    
+    await interaction.response.send_message("Item market post created!",ephemeral=True)
+
 
 bot.run(TOKEN)
